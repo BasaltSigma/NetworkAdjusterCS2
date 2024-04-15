ebef5560e4367815c2ef2dc43b344e8fa60fcba0
BasaltSigma <kris91268@gmail.com>
2024-04-16 02:30:03 +1200

    Working back on the AdjusterToolSystem now with initial setup of the system

--- Mod.cs
+++ Mod.cs
@@ -9,22 +9,29 @@ using System.IO;
 using NetworkAdjusterCS2.Code;
 using Unity.Entities;
 using HarmonyLib;
+using System.Linq;
 
 namespace NetworkAdjusterCS2
 {
     /// <summary>
-    /// NOTE: Mod API structure and understanding helped by https://github.com/ST-Apps/CS2-ExtendedRoadUpgrades/tree/master <br />
+    /// NOTE: Mod API structure and understanding helped by the following mod repos:
+    /// <list type="bullet">
+    /// <item>https://github.com/ST-Apps/CS2-ExtendedRoadUpgrades/tree/master</item>
+    /// <item>https://github.com/AlphaGaming7780/ExtraLib</item>
+    /// <item>https://github.com/kosch104/CS2-ExtraNetworksAndAreas</item>
+    /// </list>
     /// Thank you very much for making your code public on Github, it was very helpful to help me learn the API as no documentation exists
-    /// as of writing this. <br /><br />
-    /// Main mod class for Network Adjuster
+    /// as of writing this. 
     /// </summary>
     public sealed class Mod : IMod
     {
         public const string MOD_NAME = "Network Adjuster";
-        public const string MOD_ICONS_ID = "nacs2";
 
         public static ILog log = LogManager.GetLogger($"{nameof(NetworkAdjusterCS2)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
         internal Setting m_activeSettings { get; private set; }
+
+        private Harmony harmony;
+        internal static string ResourcesIcons { get; private set; }
 
         /// <summary>
         /// Called by game when mod is loaded
@@ -40,11 +47,25 @@ namespace NetworkAdjusterCS2
                 log.Info($"Current mod asset at {asset.path}");
             }
 
+            FileInfo fileInfo = new FileInfo(asset.path);
+            ResourcesIcons = Path.Combine(fileInfo.DirectoryName, "Icons");
+
+            updateSystem.World.GetOrCreateSystem<AdjusterToolSystem>();
+            updateSystem.UpdateAt<AdjusterToolSystem>(SystemUpdatePhase.ToolUpdate);
+
+            harmony = new Harmony($"{nameof(NetworkAdjusterCS2)}.{nameof(Mod)}");
+            harmony.PatchAll(typeof(Mod).Assembly);
+            var patchedMethods = harmony.GetPatchedMethods().ToArray();
+            foreach (var patchedMethod in patchedMethods)
+            {
+                log.Info($"Patched Method: {patchedMethod.Module.Name}:{patchedMethod.Name}");
+            }
+
             // create and configure settings
-            m_activeSettings = new Setting(this);
-            m_activeSettings.RegisterInOptionsUI();
-            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_activeSettings));
-            AssetDatabase.global.LoadSettings(nameof(NetworkAdjusterCS2), m_activeSettings, new Setting(this));
+            //m_activeSettings = new Setting(this);
+            //m_activeSettings.RegisterInOptionsUI();
+            //GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_activeSettings));
+            //AssetDatabase.global.LoadSettings(nameof(NetworkAdjusterCS2), m_activeSettings, new Setting(this));
         }
 
         /// <summary>
@@ -58,6 +79,7 @@ namespace NetworkAdjusterCS2
                 m_activeSettings.UnregisterInOptionsUI();
                 m_activeSettings = null;
             }
+            harmony.UnpatchAll($"{nameof(NetworkAdjusterCS2)}.{nameof(Mod)}");
         }
     }
 }
